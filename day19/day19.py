8f78cbc2cff7041ef782a54f51154175f5b9903f
David Flaten <david.flaten@joinroot.com>
2024-12-19 08:45:04 -0600

    Recursive version of part 1. Not very efficient.

--- day19/day19.py
+++ day19/day19.py
@@ -2,7 +2,7 @@
 import argparse
 from functools import cmp_to_key
 
-INPUT = "small-input.txt"
+INPUT = "input.txt"
 
 def parse_towels_and_patterns_from_file(path_to_file: str) -> tuple[set[str], list[str]]:
     '''
@@ -12,7 +12,7 @@ def parse_towels_and_patterns_from_file(path_to_file: str) -> tuple[set[str], li
     patterns: list[str] = []
     with open(path_to_file, 'r') as file:
         lines = file.readlines()
-        towels = set(lines[0].strip().split(','))
+        towels = set(towel.strip() for towel in lines[0].strip().split(','))
         for index, line in enumerate(lines):
             if index >= 2:
                 patterns.append(line.strip())
@@ -23,16 +23,17 @@ def can_create_pattern(towels: set[str], towel_pattern: str) -> bool:
     # check if a substring from the towel is in the pattern, starting with the whole string
     # and then moving to smaller substrings starting at the beginning of the string.
     # If the entire string can be made by substring in towels return true else return false.
-    print(f"Checking if {towel_pattern} can be made with {towels}")
     if not towel_pattern:
         return True
     for i in range(1, len(towel_pattern) + 1):
         prefix = towel_pattern[:i]
+        #print(f"prefix, {prefix} in towels: {prefix in towels}")
         if prefix in towels:
+            #print(f"Remaining: {towel_pattern[i:]}")
             remaining = towel_pattern[i:]
             if can_create_pattern(towels, remaining):
                 return True
-    print(f"Cannot make {towel_pattern} with {towels}")
+    #print(f"Cannot make {towel_pattern} with {towels}")
     return False
 
 def find_possible_count_of_patterns(towels: set[str], towel_patterns: list[str]) -> int:
@@ -50,8 +51,6 @@ if __name__ == "__main__":
     parser.add_argument("--find_possible_pattern_count", action='store_true', help="Find all possible patterns you can create with input.")
     args = parser.parse_args()
     towels, towel_patterns= parse_towels_and_patterns_from_file(INPUT)
-    print(towels)
-    print(towel_patterns)
     if args.find_possible_pattern_count:
         result = find_possible_count_of_patterns(towels, towel_patterns)
         print(f"Total number of possible patterns is {result}")
